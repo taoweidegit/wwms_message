612586e1b5aa0dec2c161f8c4c4251f0d1442012
Your Name <you@example.com>
2024-02-03 21:19:09 +0800

    更改

--- src/main/java/com/wmms/mutils/configuration/QuartzConfig.java
+++ src/main/java/com/wmms/mutils/configuration/QuartzConfig.java
@@ -8,25 +8,52 @@ import org.springframework.context.annotation.Configuration;
 public class QuartzConfig
 {
     @Bean
-    public JobDetail jobDetail()
+    public JobDetail jobPassDetail()
     {
         //指定任务描述具体的实现类
         return JobBuilder.newJob(SMSJob.class)
                 // 指定任务的名称
-                .withIdentity("sms")
+                .withIdentity("sms_pass")
                 // 任务描述
                 .withDescription("发送短信")
+                .usingJobData("isPass", true)
                 // 每次任务执行后进行存储
                 .storeDurably()
                 .build();
     }
 
     @Bean
-    public Trigger trigger() {
+    public JobDetail jobNotPassDetail()
+    {
+        //指定任务描述具体的实现类
+        return JobBuilder.newJob(SMSJob.class)
+                // 指定任务的名称
+                .withIdentity("sms_not_pass")
+                // 任务描述
+                .withDescription("发送短信")
+                .usingJobData("isPass", false)
+                // 每次任务执行后进行存储
+                .storeDurably()
+                .build();
+    }
+
+    @Bean
+    public Trigger passTrigger() {
         //创建触发器
         return TriggerBuilder.newTrigger()
                 // 绑定工作任务
-                .forJob(jobDetail())
+                .forJob(jobPassDetail())
+                // 每隔 5 秒执行一次 job
+                .withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(20))
+                .build();
+    }
+
+    @Bean
+    public Trigger notPassTrigger() {
+        //创建触发器
+        return TriggerBuilder.newTrigger()
+                // 绑定工作任务
+                .forJob(jobNotPassDetail())
                 // 每隔 5 秒执行一次 job
                 .withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(20))
                 .build();
--- src/main/java/com/wmms/mutils/configuration/SMSJob.java
+++ src/main/java/com/wmms/mutils/configuration/SMSJob.java
@@ -1,30 +1,122 @@
 package com.wmms.mutils.configuration;
 
+import com.aliyun.dysmsapi20170525.Client;
+import com.aliyun.dysmsapi20170525.models.SendSmsRequest;
+import com.aliyun.dysmsapi20170525.models.SendSmsResponse;
+import com.aliyun.teaopenapi.models.Config;
+import com.aliyun.teautil.models.RuntimeOptions;
+import com.google.gson.Gson;
+import com.wmms.mutils.entity.*;
+import com.wmms.mutils.mapper.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.flowable.engine.HistoryService;
 import org.flowable.engine.ProcessEngine;
-import org.flowable.engine.ProcessEngineConfiguration;
-import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.flowable.engine.TaskService;
+import org.flowable.engine.history.HistoricProcessInstance;
+import org.flowable.task.api.Task;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 public class SMSJob extends QuartzJobBean
 {
+    private static final Logger logger = LogManager.getLogger(SMSJob.class.getName());
+
+    @Resource
+    private ProcessEngine processEngine;
+
+    @Resource
+    private TaskService taskService;
+
+    @Resource
+    private HistoryService historyService;
+
+    @Resource
+    private TUserMapper userMapper;
+
+    @Resource
+    private TWareMapper wareMapper;
+
+    @Resource
+    private TApplyMapper applyMapper;
+
+    @Resource
+    private TModelMapper modelMapper;
+
+    @Resource
+    private TWareKindMapper kindMapper;
+
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException
     {
-        ProcessEngineConfiguration processEngineConfiguration = new StandaloneProcessEngineConfiguration()
-                .setJdbcUrl("jdbc:mysql://localhost:3306/wmms")
-                .setJdbcUsername("root")
-                .setJdbcPassword("19951017i")
-                .setJdbcDriver("com.mysql.cj.jdbc.Driver")
-                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
-        ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
-        HistoryService historyService = processEngine.getHistoryService();
-        historyService.createHistoricTaskInstanceQuery().list().forEach(e -> {
-            System.out.println(e.getExecutionId());
-        });
+        JobDataMap jobDataMap = context.getMergedJobDataMap();
+        boolean isPass = jobDataMap.getBoolean("isPass");
+        String taskAssignee = isPass ? "sys_notify_pass" : "sys_notify_not_pass";
 
-        System.out.println("定时任务");
+        taskService = processEngine.getTaskService();
+        historyService = processEngine.getHistoryService();
+        List<Task> taskList = taskService.createTaskQuery()
+                            .taskAssignee(taskAssignee)
+                            .includeProcessVariables()
+                            .includeTaskLocalVariables()
+                            .list();
+        for (Task task: taskList)
+        {
+            HistoricProcessInstance historicProcessInstance = historyService
+                                                            .createHistoricProcessInstanceQuery()
+                                                            .processInstanceId(task.getProcessInstanceId())
+                                                            .singleResult();
+            String uid = historicProcessInstance.getStartUserId();
+
+            TUser tUser = userMapper.selectByPrimaryKey(Long.valueOf(uid));
+            String mobile = tUser.getMobile(), name = tUser.getName();
+
+            String formId = task.getProcessVariables().get("form").toString();
+            TApply apply = applyMapper.selectByPrimaryKey(Long.valueOf(formId));
+            TWare ware = wareMapper.selectByPrimaryKey(apply.getWare());
+            TModel model = modelMapper.selectByPrimaryKey(ware.getModel());
+            TWareKind wareKind = kindMapper.selectByPrimaryKey(model.getKind());
+            String modelName = model.getName(), kind = wareKind.getName();
+
+            Config config = new Config()
+                    // 必填，您的 AccessKey ID
+                    .setAccessKeyId("LTAI5tFL2ZNX4etwRnV65GkJ")
+                    // 必填，您的 AccessKey Secret
+                    .setAccessKeySecret("pCSMAFuTbLKbGmBvr7Ad0OXJsKZx97");
+            // Endpoint 请参考 https://api.aliyun.com/product/Dysmsapi
+            config.endpoint = "dysmsapi.aliyuncs.com";
+
+            Gson gson = new Gson();
+            Map<String, String> map = new HashMap<>();
+            map.put("name", name);
+            map.put("ware", kind + " " + modelName);
+            map.put("result", isPass ? "通过" : "被拒");
+
+            try
+            {
+                Client client = new Client(config);
+                SendSmsRequest sendSmsRequest = new SendSmsRequest()
+                        .setPhoneNumbers(mobile)
+                        .setSignName("tdu个人开发")
+                        .setTemplateCode("SMS_465135391")
+                        .setTemplateParam(gson.toJson(map));
+                SendSmsResponse sendSmsResponse = client.sendSmsWithOptions(sendSmsRequest, new RuntimeOptions());
+                String code = sendSmsResponse.body.code;
+                if (Objects.equals(code, "OK"))
+                    taskService.complete(task.getId());
+            }
+            catch (Exception e)
+            {
+                logger.error(e.getMessage());
+            }
+        }
     }
 }
