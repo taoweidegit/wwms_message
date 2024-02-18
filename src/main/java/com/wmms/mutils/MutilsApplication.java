3c2172b643b49f0e0b89f07c5499f4a3e59498f3
Your Name <you@example.com>
2024-02-18 22:50:54 +0800

    消息队列

--- src/main/java/com/wmms/mutils/MutilsApplication.java
+++ src/main/java/com/wmms/mutils/MutilsApplication.java
@@ -3,14 +3,14 @@ package com.wmms.mutils;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @MapperScan("com.wmms.mutils.mapper")
-@EnableJms
-public class MutilsApplication {
+public class MutilsApplication
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(MutilsApplication.class, args);
     }
 
